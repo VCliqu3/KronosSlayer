8ebd322f9c9d1be9a0c9828c8a89a34150a11f81
Raphael <115659111+VCliqu3@users.noreply.github.com>
2022-10-23 10:28:40 -0500

    Cambios
    No detenerse cuando se recibe daño y se esta dasheando

--- Assets/Scripts/Levels/Player/Behaviors/GetHurt/GetHurtBehavior.cs
+++ Assets/Scripts/Levels/Player/Behaviors/GetHurt/GetHurtBehavior.cs
@@ -18,7 +18,7 @@ public class GetHurtBehavior : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        _movementController.Stop();
+        //_movementController.Stop();
 
         if (!_healthController.isHurting)
         {
