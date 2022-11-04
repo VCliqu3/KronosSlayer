bf5a99f46ad3b650ae8d4ec7d468dcd011d6cab9
Raphael <rpanduro.t@gmail.com>
2022-11-04 13:08:10 -0500

    :/

--- Assets/Scripts/Levels/Enemies/BasicEnemyDeathBehavior.cs
+++ Assets/Scripts/Levels/Enemies/BasicEnemyDeathBehavior.cs
@@ -18,10 +18,10 @@ public class BasicEnemyDeathBehavior : StateMachineBehaviour
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _basicEnemyMovementController.Stop();
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
