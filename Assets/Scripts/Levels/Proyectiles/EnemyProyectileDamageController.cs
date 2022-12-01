bbc638311ccb78467d3133e1fb10ff675623586a
Raphael <115659111+VCliqu3@users.noreply.github.com>
2022-12-01 10:35:57 -0500

    ShieldTakeDamage VFX

--- Assets/Scripts/Levels/Proyectiles/EnemyProyectileDamageController.cs
+++ Assets/Scripts/Levels/Proyectiles/EnemyProyectileDamageController.cs
@@ -6,12 +6,21 @@ public class EnemyProyectileDamageController : MonoBehaviour
 {
     public float damage;
     public float shieldPenetration;
+
+    public GameObject ShieldImpactVFX;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             if (!collision.GetComponent<HealthController>().invincibilityEnabled) 
             {
+                if(collision.GetComponent<HealthController>().shield > 0)
+                {
+                    GameObject ShImpVFX = Instantiate(ShieldImpactVFX, transform.position, transform.rotation);
+                    ShImpVFX.transform.parent = collision.transform;
+                    Destroy(ShImpVFX, 1.2f);
+                }
+
                 collision.GetComponent<HealthController>().TakeDamage(damage, shieldPenetration);
                 Destroy(gameObject);
             }
--- Assets/Scripts/Levels/Proyectiles/PlayerProyectileDamageController.cs
+++ Assets/Scripts/Levels/Proyectiles/PlayerProyectileDamageController.cs
@@ -7,12 +7,22 @@ public class PlayerProyectileDamageController : MonoBehaviour
     /*[SerializeField] Player;*/
     public float damage;
     public float shieldPenetration;
+
+    public GameObject ShieldImpactVFX;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
             if (!collision.GetComponent<BasicEnemyHealthController>().isDead)
             {
+                if (collision.GetComponent<BasicEnemyHealthController>().shield > 0)
+                {
+                    GameObject ShImpVFX = Instantiate(ShieldImpactVFX, transform.position, transform.rotation);
+                    ShImpVFX.transform.parent = collision.transform;
+                    Destroy(ShImpVFX, 1.2f);
+                }
+
                 collision.GetComponent<BasicEnemyHealthController>().TakeDamage(damage, shieldPenetration);
                 Destroy(gameObject);
             }
@@ -22,6 +32,12 @@ public class PlayerProyectileDamageController : MonoBehaviour
         {
             if (!collision.GetComponent<KKHealthController>().isDead && collision.GetComponent<KKHealthController>().canTakeDamage)
             {
+                if (collision.GetComponent<KKHealthController>().shield > 0)
+                {
+                    GameObject ShImpVFX = Instantiate(ShieldImpactVFX, transform.position, transform.rotation);
+                    ShImpVFX.transform.parent = collision.transform;
+                    Destroy(ShImpVFX, 1.2f);
+                }
                 collision.GetComponent<KKHealthController>().TakeDamage(damage, shieldPenetration);
                 Destroy(gameObject);
             }
