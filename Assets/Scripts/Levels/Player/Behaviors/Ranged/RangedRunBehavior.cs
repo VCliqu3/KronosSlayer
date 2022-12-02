9b3175c6224bdd364e1750fe49c47086329ebe09
Raphael <115659111+VCliqu3@users.noreply.github.com>
2022-12-02 09:40:02 -0500

    Player_Footsteps

--- Assets/Scripts/Levels/Player/Behaviors/Ranged/RangedRunBehavior.cs
+++ Assets/Scripts/Levels/Player/Behaviors/Ranged/RangedRunBehavior.cs
@@ -10,20 +10,63 @@ public class RangedRunBehavior : StateMachineBehaviour
     public string WalkingSFXName;
     private bool isPlayingWalkingSFX = false;
 
+    public string footstep1SFXName;
+    public string footstep2SFXName;
+
+    public bool individualFootsteps;
+
+    public float timeBeetweenFootsteps = 0.2f;
+    public float timeforFirtsFootstep = 0.2f;
+
+    private float time = 0f;
+
+    private int footstepNumber = 1;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _movementController = animator.gameObject.GetComponent<MovementController>();
-        _rangedController = animator.gameObject.GetComponent<RangedController>();        
+        _rangedController = animator.gameObject.GetComponent<RangedController>();
+
+        time = timeBeetweenFootsteps-timeforFirtsFootstep;
+        footstepNumber = 1;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (!isPlayingWalkingSFX)
+        if (individualFootsteps)
         {
-            AudioManager.instance.PlayPerpetualSFX(WalkingSFXName);
-            isPlayingWalkingSFX = true;
+            time += Time.deltaTime;
+
+            if (time >= timeBeetweenFootsteps)
+            {
+                time = 0;
+                switch (footstepNumber)
+                {
+                    case 1:
+                        AudioManager.instance.PlaySFX(footstep1SFXName);
+                        footstepNumber = 2;
+                        break;
+                    case 2:
+                        AudioManager.instance.PlaySFX(footstep2SFXName);
+                        footstepNumber = 1;
+                        break;
+                    default:
+                        AudioManager.instance.PlaySFX(footstep1SFXName);
+                        footstepNumber = 2;
+                        break;
+                }
+            }
+
+        }
+        else
+        {
+            if (!isPlayingWalkingSFX)
+            {
+                AudioManager.instance.PlayPerpetualSFX(WalkingSFXName);
+                isPlayingWalkingSFX = true;
+            }
         }
 
         if (PauseController.gamePaused)
@@ -58,6 +101,8 @@ public class RangedRunBehavior : StateMachineBehaviour
     {
         AudioManager.instance.StopPerpetualSFX();
         AudioManager.instance.StopPerpetualSFX(); isPlayingWalkingSFX = false;
+
+        time = 0f;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
